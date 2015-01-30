310622f7d0e688772efab39fd1cad28282adf118
koska182 <aleksandar.kosanovic@gmail.com>
2015-01-30 17:10:09 +0100

    Update PWM_renc_Tiny85.c

--- PWM_renc_Tiny85.c
+++ PWM_renc_Tiny85.c
@@ -1,68 +1,59 @@
 /*
- * PWM_Encoder.c
+ * Tiny85_pwm_enc.c
  *
- * Created: 29.1.2015. 13:49:52
+ * Created: 30.1.2015. 9:30:57
  *  Author: akosanovic
  */ 
+ 
 #define F_CPU 8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
-#include<avr/interrupt.h>
+#include <avr/interrupt.h>
 
-uint16_t enc = 0;
+volatile uint16_t enc = 127;
 
-// Subroutine declarations
-void initInterrupts(void);
 
 int main(void)
 {
-	DDRB   |= (1 << PB1);                   // PWM output on PB1 OCR1A
+	DDRB   |= (1 << PB0);                   // PWM output on PB0 OCR0A
 	// phase correct PWM, 8-bit
 	TCCR0A = (1 << COM0A1)| (1 << WGM02) | (1 << WGM00);
-	OCR1A = 0x00;                         // initial PWM pulse width
+	OCR0A = 0x00;                         // initial PWM pulse width
 	// clock source = clock / 1 ; start PWM
 	TCCR0B = (1 << CS00);
 	
-	while(1)
-	{
-		//OCR1A  = 0x3FF;     // maximum 10-bit value
-	}
-}
-
-// Routine to setup INT0
-void initInterrupts(void)
-{
-	// Assure that pin PB2 (INT0) and PB0 are inputs
+	// Assure that pin PB2 and PB1 are inputs
 	DDRB &= ~(1<<PB2);
-	DDRB &= ~(1<<PB0);
+	DDRB &= ~(1<<PB1);
 
 	// Enable the pull-up resistors
-	PORTB |= (1<<PB2)|(1<<PB0);
-
-	// Falling edge in INT0 (PB0 / pin7) to cause interrupt
-	MCUCR |= (1<<ISC01);
-
-	// Enable and INT0
-	GIMSK |= (1<<INT0);
+	PORTB |= (1<<PB2)|(1<<PB1);
+	
+	GIMSK = 0b00100000;		// Enable and PCINT
+	PCMSK = 0b00000010;		// Enable PCINT1
+	sei();
+	
+	
+	while(1)
+	{
+	  OCR0A = enc;
+	}
 }
 
-// The Interrupt Service Routine for external INT1
-ISR(INT0_vect)
-{
-	// When an interrupt occurs, we only have to check the level of
-	// of pin PB0 to determine the direction
-	if (PINB & _BV(PB0)){
-	// Increase enc
-		if (enc<1024){
-			enc++;
-			OCR1A = enc;
+ISR(PCINT0_vect)
+{	 
+	if( (PINB & (1 << PINB1)) == 0 ){	//falling edge
+	  	// When an interrupt occurs, we only have to check the level of
+	  	// of pin PB2 to determine the direction
+	  if(PINB & _BV(PB2)){
+			// Increase enc
+			if (enc<255)enc++;
 			}
-	}
-	else{
-		if (enc>0){
-		// Decrease enc
-		enc--;
-		OCR1A = enc;
-		}
+		
+		else{
+			// Decrease enc
+			if (enc>0)enc--;
+			}
+			
 	}
 }
